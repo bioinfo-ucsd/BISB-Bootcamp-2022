ad382156c6d0b88b3fb18d8c70fe1c8c287f5a90
mikecuoco <mcuoco@ucsd.edu>
2022-09-12 12:24:23 -0700

    remove jbrowse check, remove cd $HOME

--- modules/02-sequencing/student_setup.sh
+++ modules/02-sequencing/student_setup.sh
@@ -5,22 +5,19 @@
 # exit if any non-zero, exit if undefined var
 set -euo pipefail
 
-cd $HOME
 source_dir=$(dirname "$0")
 dest_dir="$HOME/bootcamp-02-sequencing"
 
 this_dir="$dest_dir/01-whole-exome"
-echo "creating $this_dir..."
-mkdir -p $this_dir 
-cp $source_dir/01-whole-exome.ipynb $this_dir/activity.ipynb
+mkdir -p $this_dir && echo "creating $this_dir..."
+cp "$source_dir/01-whole-exome.ipynb" "$this_dir/activity.ipynb"
 ln -s $source_dir/img $this_dir/img
 ln -s $source_dir/data/wes* $this_dir/
 ln -s $source_dir/ref $this_dir/ref
 
 this_dir="$dest_dir/02-whole-genome"
-echo "creating $this_dir..."
-mkdir -p $this_dir
-cp $source_dir/02-whole-genome.ipynb $this_dir/activity.ipynb
+mkdir -p $this_dir && echo "creating $this_dir..."
+cp "$source_dir/02-whole-genome.ipynb" "$this_dir/activity.ipynb"
 ln -s $source_dir/img $this_dir/img
 ln -s $source_dir/data/wgs* $this_dir/
 ln -s $source_dir/ref $this_dir/ref
